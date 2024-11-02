cfa511d3e9cc6f4249d88fb597fc0d7958b77f6c
lucasbp6 <lucasbatistapereira06@gmail.com>
2024-11-02 15:06:39 -0300

    esqueleto menu

--- src/menu.py
+++ src/menu.py
@@ -4,19 +4,26 @@ LARGURA, ALTURA = 800, 600
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 GREEN = (0,255,0)
+
+pygame.init()
+FONTE = pygame.font.SysFont('arial', 30, False, False)
+FONTE2 = pygame.font.SysFont('arial', 70, False, False)
+TELA = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption('Menu')
 # Parametrizar tudo, deixando em classes
+
 class Text:
 
-    def __init__(self,texto,  fonte, posicao_y, cor, cor_box, borda):
+    def __init__(self,texto,  fonte, posicao_y, cor, cor_box, borda, posicao_x = None):
         self.render = fonte.render(texto, True, cor)
         self.cor = cor
         self.cor_box = cor_box
         self.tamanho = self.render.get_size()
-        self.posicao = ((LARGURA - self.tamanho[0])/2, posicao_y)
+        if posicao_x == None:
+            self.posicao = ((LARGURA - self.tamanho[0])/2, posicao_y)
+        else:
+            self.posicao = (posicao_x, posicao_y)
         self.box = pygame.Rect(self.posicao[0] - borda, self.posicao[1] - borda, self.tamanho[0] + 2*borda, self.tamanho[1] + 2*borda)
-    
-    #def box(self, borda):
-    #    pygame.Rect(self.posicao[0] - borda, self.posicao[1] - borda, self.tamanho[0] + 2*borda, self.tamanho[1] + 2*borda)
 
     def desenhar(self, tela):
         tela.blit(self.render, self.posicao)
@@ -33,74 +40,161 @@ class Text:
 
     def on_click(self, click):
         if self.box.collidepoint(click):
-            print("apertou")
-
-
-pygame.init()
-tela = pygame.display.set_mode((LARGURA, ALTURA))
-pygame.display.set_caption('Menu')
-#pygame.clock.tick(30)
-running = True
-cora = (0,0,0)
-corb = (0,255,0)
-fonte = pygame.font.SysFont('arial', 30, False, False)
-fonte2 = pygame.font.SysFont('arial', 70, False, False)
-cor4 = cora
-
-titulo = Text("Jogo feito pelo grupo",fonte2, 110, WHITE, GREEN,  10)
-
-
-txt = fonte.render("Novo jogo", True, (255,255,255))
-r_l, r_a = txt.get_size()
-rect = pygame.Rect((LARGURA - r_l)/2 -5, 275,r_l+10, r_a+10 )
-
-txt2 = fonte.render("Carregar", True, (255,255,255))
-r_l2, r_a2 = txt2.get_size()
-rect2 = pygame.Rect((LARGURA - r_l2)/2 -5, 325,r_l2+10, r_a2+10 )
-
-txt3 = fonte.render("Sair", True, (255,255,255))
-r_l3, r_a3 = txt3.get_size()
-rect3 = pygame.Rect((LARGURA - r_l3)/2 -5, 375,r_l3+10, r_a3+10 )
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            click_pos = event.pos
-            if rect.collidepoint(click_pos):
-                print("novo jogo")
-            if rect2.collidepoint(click_pos):
-                print('carregar')
-            if rect3.collidepoint(click_pos):
-                running = False
-    pos = pygame.mouse.get_pos() 
+            return True
+        return False
     
-    if rect.collidepoint(pos):
-        cor = corb
-    else:
-        cor = cora
+def selecao_personagem(tela):
+    p1 = pygame.Rect(LARGURA/10, ALTURA/3, LARGURA/5, ALTURA/3)
+    p2 = pygame.Rect(4*LARGURA/10, ALTURA/3, LARGURA/5, ALTURA/3)
+    p3 = pygame.Rect(7*LARGURA/10, ALTURA/3, LARGURA/5, ALTURA/3)
+    
+    pygame.draw.rect(tela, GREEN, p1)
+    pygame.draw.rect(tela, GREEN, p2)
+    pygame.draw.rect(tela, GREEN, p3)
 
-    if rect2.collidepoint(pos):
-        cor2 = corb
-    else:
-        cor2 = cora
-    if rect3.collidepoint(pos):
-        cor3 = corb
-    else:
-        cor3 = cora
-    tela.fill((0,0,0))
 
-    titulo.on_contact(pos, tela)
-    pygame.draw.rect(tela, cor2, rect2)
-    pygame.draw.rect(tela, cor3, rect3)
-    pygame.draw.rect(tela, cor, rect)
+def novo_jogo(tela):
+    titulo = Text("Escolha seu persongem",FONTE2, 80, WHITE, GREEN,  10)
+    back = Text("Voltar",FONTE, ALTURA-35, WHITE, GREEN, 5, LARGURA - 100)
 
-    titulo.desenhar(tela)
-    #tela.blit(titulo, ((LARGURA - t_l)/2 , 100 + 10))
-    tela.blit(txt, ((LARGURA - r_l)/2, 280))
-    tela.blit(txt2, ((LARGURA - r_l2)/2, 330))
-    tela.blit(txt3, ((LARGURA - r_l3)/2, 380))
-    pygame.display.flip()
+    p1 = pygame.Rect(LARGURA/10, ALTURA/3, LARGURA/5, ALTURA/3)
+    p2 = pygame.Rect(4*LARGURA/10, ALTURA/3, LARGURA/5, ALTURA/3)
+    p3 = pygame.Rect(7*LARGURA/10, ALTURA/3, LARGURA/5, ALTURA/3)
+    selection = True
+    while selection:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = event.pos
+                if p1.collidepoint(click):
+                    return 'p1'
+                if p2.collidepoint(click):
+                    return 'p2'
+                if p3.collidepoint(click):
+                    return 'p3'
+                if back.on_click(click):
+                    selection = False
+        pos = pygame.mouse.get_pos() 
+        tela.fill(BLACK)
+    
+        pygame.draw.rect(tela, GREEN, p1)
+        pygame.draw.rect(tela, GREEN, p2)
+        pygame.draw.rect(tela, GREEN, p3)
+
+        back.on_contact(pos, tela)
+        titulo.desenhar(tela)
+        back.desenhar(tela)
+        pygame.display.flip()
+    return
+
+def verifica_codigo(codigo):
+    if codigo == "1111":
+        print(codigo)
+        return True
+    else:
+        codigo = ''
+        return False
+
+def carregar(tela):
+    titulo = Text("Digite o codigo:", FONTE2, 120, WHITE, GREEN, 10)
+    back = Text("Voltar", FONTE, ALTURA-35, WHITE, GREEN, 5, LARGURA - 100)
+    enter = Text("Enviar", FONTE, ALTURA/2 + 50, WHITE, GREEN, 5)
+    erro_t = Text("Erro, codigo invalido", FONTE, ALTURA/2 + 100, WHITE, GREEN, 5)
+    input = pygame.Rect(LARGURA/2 - 85, ALTURA/2 - 35, 170, 70)
+    codigo = ''
+    ativo = False  # Inicialize 'ativo' aqui
+    carregando = True
+    m_erro = False
+    while carregando:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input.collidepoint(event.pos):
+                    ativo = not ativo
+                else:
+                    ativo = False
+                if back.on_click(event.pos):
+                    carregando = False
+                if enter.on_click(event.pos):
+                    if not verifica_codigo(codigo):
+                        m_erro = True 
+                    else:
+                        pass
+                        #fazer com que entre no jogo
+                    codigo = ''
+                
+            if event.type == pygame.KEYDOWN:
+                if ativo:
+                    if event.key == pygame.K_RETURN:
+                        if not verifica_codigo(codigo):
+                            m_erro = True 
+                        else:
+                            pass
+                            #fazer com que entre no jogo
+                        codigo = ''
+                    elif event.key == pygame.K_BACKSPACE:
+                        codigo = codigo[:-1]  
+                    elif len(codigo) > 3:
+                        pass
+                    else:
+                        codigo += event.unicode  
+
+        codigo_render = FONTE2.render(codigo, True, WHITE)
+
+        tela.fill(BLACK)
+        pos = pygame.mouse.get_pos() 
+
+        tela.blit(codigo_render, (input.x + 5, input.y + 5))
+        pygame.draw.rect(tela, GREEN, input, 2)
+        if m_erro:
+            erro_t.desenhar(tela)
+        enter.on_contact(pos, tela)
+        back.on_contact(pos, tela)
+        titulo.desenhar(tela)
+        back.desenhar(tela)
+        enter.desenhar(tela)
+        pygame.display.flip()
+
+    return
+
+def hub(tela):
+    running_hub = True
+    titulo = Text("Jogo feito pelo grupo", FONTE2, 110, WHITE, GREEN,  10)
+    txt = Text("Novo jogo", FONTE, 275, WHITE, GREEN, 5)
+    txt2 = Text("Carregar", FONTE, 325, WHITE, GREEN, 5)
+    txt3 = Text("Sair", FONTE, 375, WHITE, GREEN, 5)
+    while running_hub:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running_hub = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click_pos = event.pos
+                if txt.on_click(click_pos):
+                    personagem = novo_jogo(tela)
+                    print(personagem)
+                    #iniciar game
+                if txt2.on_click(click_pos):
+                    carregar(tela)
+                if txt3.on_click(click_pos):
+                    running_hub = False
+
+        pos = pygame.mouse.get_pos() 
+
+        tela.fill((0,0,0))
+
+        txt.on_contact(pos, tela)
+        txt2.on_contact(pos,tela)
+        txt3.on_contact(pos,tela)
+
+        titulo.desenhar(tela)
+        txt.desenhar(tela)
+        txt2.desenhar(tela)
+        txt3.desenhar(tela)
+
+        pygame.display.flip()
+
+hub(TELA)
 
 pygame.quit()
