6dd8f6c52e7a01af87b82fd28f5ce1e6fe27d398
SamyraMara <samyra@DESKTOP-V3CITE4>
2024-12-02 22:47:08 -0300

    removendo envio incorreto

--- branch_samyra_arquivos_antigos/src/main.py
+++
@@ -1,5 +0,0 @@
-from jogo import Jogo
-
-if __name__ == "__main__":
-    jogo = Jogo()
-    jogo.rodar()
--- branch_samyra_arquivos_antigos/src/mapa.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from config import COR_PAREDE
-
-class Parede(pygame.sprite.Sprite):
-    def __init__(self, x, y, largura, altura):
-        super().__init__()
-        self.image = pygame.Surface((largura, altura))
-        self.image.fill(COR_PAREDE)
-        self.rect = self.image.get_rect(topleft=(x, y))
